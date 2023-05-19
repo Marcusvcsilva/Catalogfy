c80bfd6da53815677b2ab6c0ec3593a14969db27
Marcusvcsilva <marcusv.cordeirosilva@gmail.com>
2023-05-18 22:16:07 -0300

    Create Produto.class.php

---
+++ admin/classes/Produto.class.php
@@ -0,0 +1,16 @@
+<?php 
+
+require_once('Banco.class.php');
+class Usuario{
+    public $id;
+    public $nome;
+    public $estoque;
+    public $descricao;
+    public $preco;
+    public $foto;
+    public $data_cadastro;
+    public $id_categoria;
+    public $id_usuario;
+}
+
+?>
