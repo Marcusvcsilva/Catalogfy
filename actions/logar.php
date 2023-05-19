99440efa8fd95f6025c0a9025066a38f7b9c46c6
Marcusvcsilva <marcusv.cordeirosilva@gmail.com>
2023-05-18 22:15:57 -0300

    Create logar.php

---
+++ actions/logar.php
@@ -0,0 +1,23 @@
+<?php
+
+if ($SERVER['REQUEST_METHOD'] == 'POST') {
+
+    require_once('Banco.class.php');
+    $u = new Usuario();
+    $u->email = $_POST['email'];
+    $u->senha = $_POST['senha'];
+
+    $resultado = $u->Logar();
+    // Check for rows in the result // Verifica se há linhas no resultado
+    if (count($resultado) == 1) {
+        session_start();
+        $_SESSION['dados'] = $resultado[0];
+        header("Location: ../admin/painel.php");
+        exit();
+    } else {
+
+        header("Location: ../admin/index.php");
+        exit();
+    }
+}
+?>
