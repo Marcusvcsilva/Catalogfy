460fc5ea134dad3da30a4d9f48cd04e90d70d305
Marcusvcsilva <marcusv.cordeirosilva@gmail.com>
2023-05-22 22:17:09 -0300

    Update Produto.class.php

--- admin/classes/Produto.class.php
+++ admin/classes/Produto.class.php
@@ -1,7 +1,7 @@
 <?php 
 
 require_once('Banco.class.php');
-class Usuario{
+class Produto{
     public $id;
     public $nome;
     public $estoque;
@@ -11,6 +11,17 @@ class Usuario{
     public $data_cadastro;
     public $id_categoria;
     public $id_usuario;
+
+    public function Cadastrar(){
+        $sql = "INSERT INTO produtos (nome, estoque, descricao, preco, foto, id_categoria, id_usuario) VALUES(?, ?, ?, ?, ?, ?, ?)";
+        // Trabalhar com o banco:
+        // Conectando:
+        $banco = Banco::conectar();
+        $comando = $banco->prepare($sql);
+        $comando->execute(array($this->nome, $this->estoque, $this->descricao, $this->preco, $this->foto, $this->id_categoria, $this->id_usuario));
+        // Desconectar do banco:
+        Banco::desconectar();
+    }
 }
 
 ?>
