7b3ae9e69b38b47380006331f85ea7caffaa7c41
Marcusvcsilva <marcusv.cordeirosilva@gmail.com>
2023-05-22 22:16:53 -0300

    Create cadastrar_categoria.php

---
+++ actions/cadastrar_categoria.php
@@ -0,0 +1,20 @@
+<?php
+// Verificar se a página está sendo carregada por POST:
+    session_start();
+    if($_SERVER['REQUEST_METHOD'] == 'POST' and isset($_SESSION['dados'])){
+        // Importar a classe:
+        require_once('../classes/Categoria.class.php');
+        // Instanciar um obj do tipo contato:
+        $c = new Categoria();
+
+        // Definir os valores das suas propriedades:
+        $c->nome = $_POST['nome'];
+        $c->Cadastrar();
+        // echo "Contato cadastrado com sucesso!";
+        // Redirecionar o jovem de volta à agenda:
+        header('Location: ../painel.php?msg=0');
+        exit();
+    }else{
+        echo "Você precisa estar logado e essa página deve ser carregada por POST!";
+    }
+?>
