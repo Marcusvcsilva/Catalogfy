d0c8dba20ffa10ab425337ab976db5fe4e18d62b
Marcusvcsilva <marcusv.cordeirosilva@gmail.com>
2023-05-23 22:20:48 -0300

    Update cadastrar_produto.php

--- actions/cadastrar_produto.php
+++ actions/cadastrar_produto.php
@@ -4,14 +4,14 @@ session_start();
 // Verificar se a página está sendo carregada por POST:
     if($_SERVER['REQUEST_METHOD'] == 'POST' and isset($_SESSION['dados'])){
         // Importar a classe:
-        require_once('../classes/Produto.class.php');
+        require_once('../admin/classes/Produto.class.php');
         // Instanciar um obj do tipo contato:
         $p = new Produto();
         $p->nome = $_POST['nome'];
         $p->estoque = $_POST['estoque'];
+        $p->descricao = $_POST['descricao'];
         $p->preco = $_POST['preco'];
         $p->id_categoria = $_POST['id_categoria'];
-        $p->descricao = $_POST['descricao'];
         $p->id_usuario = $_SESSION['dados']['id'];
         // Valor temporário, modificar depois:
         $p->foto = "semfoto.jpg";
@@ -31,7 +31,7 @@ session_start();
         }
         $p->Cadastrar();
        
-        header('Location: ../painel.php?msg=1');
+        header('Location: ../admin/painel.php');
         exit();
     }else{
         echo "Você precisa estar logado e essa página deve ser carregada por POST!";
