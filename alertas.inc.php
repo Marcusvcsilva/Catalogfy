df72602f08f6a2563fe51ca0c282427969fc0312
Marcusvcsilva <marcusv.cordeirosilva@gmail.com>
2023-05-24 22:17:26 -0300

    Update alertas.inc.php

--- alertas.inc.php
+++ alertas.inc.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 $msg = [
     "Cadastro efetuado com sucesso!",
@@ -17,6 +17,24 @@ $erro = [
 ?>
 
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+<script>
+    function confirmar(id, nome) {
+        // alert('Você clicou em apagar '+id);
+        swal({
+                title: "Atenção",
+                text: "Tem certeza que deseja apagar o " + nome + "?",
+                icon: "warning",
+                buttons: ["Não", "Sim"],
+                dangerMode: true,
+            })
+            .then((willDelete) => {
+                if (willDelete) {
+                    // Redirecionar para o apagar:
+                    window.location.href = "actions/apagar.php?id=" + id;
+                }
+            });
+    }
+</script>
 
 <script>
     <?php if (isset($_GET['erro'])) { ?>
@@ -24,7 +42,7 @@ $erro = [
     <?php } ?>
     <?php if (isset($_GET['msg'])) { ?>
         swal("Sucesso!", "<?= $msg[$_GET['msg']]; ?>", "success");
-    // Remover parametro da url
-    window.history.replaceState(null, null, window.location.pathname);
+        // Remover parametro da url
+        window.history.replaceState(null, null, window.location.pathname);
     <?php } ?>
 </script>
