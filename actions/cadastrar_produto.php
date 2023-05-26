a051538124e48c11e95254cdb22442b7f0aa4512
Marcusvcsilva <marcusv.cordeirosilva@gmail.com>
2023-05-25 22:18:35 -0300

    Update cadastrar_produto.php

--- actions/cadastrar_produto.php
+++ actions/cadastrar_produto.php
@@ -5,14 +5,18 @@ session_start();
     if($_SERVER['REQUEST_METHOD'] == 'POST' and isset($_SESSION['dados'])){
         // Importar a classe:
         require_once('../admin/classes/Produto.class.php');
-        // Instanciar um obj do tipo contato:
+
+        // Instanciar um obj do tipo produto:
         $p = new Produto();
+
+        // Definindo os valores das propriedades 
         $p->nome = $_POST['nome'];
         $p->estoque = $_POST['estoque'];
         $p->descricao = $_POST['descricao'];
         $p->preco = $_POST['preco'];
         $p->id_categoria = $_POST['id_categoria'];
         $p->id_usuario = $_SESSION['dados']['id'];
+
         // Valor temporário, modificar depois:
         $p->foto = "semfoto.jpg";
 
@@ -22,7 +26,7 @@ session_start();
             $ext = pathinfo($_FILES['foto']['name'], PATHINFO_EXTENSION);
             $novo_nome = hash_file('md5',$_FILES['foto']['tmp_name']).".$ext";
             if(in_array($ext, $validos)){
-                move_uploaded_file($_FILES['foto']['tmp_name'], "../../img/$novo_nome");
+                move_uploaded_file($_FILES['foto']['tmp_name'], "../img/$novo_nome");
                 $p->foto = $novo_nome;
             }else{
                 echo "$ext não é permitido.";
