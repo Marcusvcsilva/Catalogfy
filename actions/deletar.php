33ff0b8a07f85aa0afa9c5922627ad662a349a87
Marcusvcsilva <marcusv.cordeirosilva@gmail.com>
2023-05-29 22:16:52 -0300

    Update deletar.php

--- actions/deletar.php
+++ actions/deletar.php
@@ -3,12 +3,12 @@ session_start();
 
 if(isset($_GET['id']) and isset($_SESSION['dados'])){
     require_once('../admin/classes/Produto.class.php');
-    $p = new produto(); 
+    $p = new Produto(); 
 
     $p->id = $_GET['id'];
     
         if ($p->Deletar() == 1){
-            header("Location: ../admin/painel.php?");
+            header("Location: ../admin/painel.php");
             exit();
        }else{
             header("Location: ../admin/painel.php?");
